7e9e54356c56565e146277bd86c52e123d8a82bc
Grace <gu5698@gmail.com>
2021-12-27 20:18:57 +0800

    Angular-s4 初始化英雄清單

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,7 +68,8 @@ export class HeroService {
 
   /** POST: add a new hero to the server */
   addHero (hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+    const postData = (this.heroes.length) ? hero: ({ name: hero.name, id: 0 })
+    return this.http.post<Hero>(this.heroesUrl, postData, httpOptions).pipe(
       tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
