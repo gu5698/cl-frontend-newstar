08d3682b9cbb43e7161320d1f3a3e8b412c59e57
Grace <gu5698@gmail.com>
2021-12-27 20:20:55 +0800

    outpoints

--- src/app/bonus-point/bonus-point.component.ts
+++ src/app/bonus-point/bonus-point.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BsJs1Service } from './bs-js1.service';
-import { of } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Component({
@@ -9,6 +9,8 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./bonus-point.component.css']
 })
 export class BonusPointComponent implements OnInit {
+  flexBoxes: string[] = [];
+  gridBoxes: string[] = [];
 
   constructor(
     private bsJs1Service: BsJs1Service,
@@ -18,7 +20,16 @@ export class BonusPointComponent implements OnInit {
 
 
   ngOnInit() {
+    this.getFlexBoxes();
+    this.getGridBoxes();
+  }
 
+  getFlexBoxes(): void {
+    this.flexBoxes = new Array(9).fill('').map((d, i) => (i !== 4)? `f${i+1}`: '')
+  }
+
+  getGridBoxes(): void {
+    this.gridBoxes = new Array(7).fill('').map((d, i) => `g${i+1}`)
   }
 
   redirectToKeiPage() {
@@ -45,6 +56,11 @@ export class BonusPointComponent implements OnInit {
     //  third
 
     // ToDo...
+    forkJoin(
+      first$,
+      second$,
+      third$
+    ).subscribe(d => console.log(d))
   }
 
 
@@ -53,7 +69,9 @@ export class BonusPointComponent implements OnInit {
     this.bsJs1Service.getSample()
       .forEachChilds((child) => {
         // ToDo : 實作你的解決方案...
-        result = 'ToDo...';
+        if (!result) result = ''
+        const ary = [...child.parent?.['child'].map(d => d.value).flat(), child.parent.value].join(' , ') 
+        result = !result.includes(ary) ? [result, ary].join(' , ').replace(/^ \, /g, '') : result
       });
 
     // 預期alert的結果 => js 1 answer : child_1_1 , child_1_2 , parent_1 , child_2_1 ,  parent_2 ,child_3_1 , child_3_2 , child_3_3 , parent_3
